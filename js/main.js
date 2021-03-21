070ad883cba89dbb9a1a04c8a2e196367ecca47a
Lucas NAURY <38406425+lucasnaury@users.noreply.github.com>
2021-03-21 16:34:53 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
+  var initialLocalImgs = new Array();
+  for(i=1;i<=50;i++){ //50 = total number of images
+    initialLocalImgs.push("img/Pictures/"+i+".jpg");
+  }
+  var actualLocalImgs = [...initialLocalImgs];
 
   var count=0;
 
@@ -10,27 +15,26 @@ $(document).ready(function () {
   var numberOfImages = 30;
   var pageNb;
   var maxPageNb;
-  var query = "cute_kitten,kitten,kitty,cute_cat,cat,cute_puppy";
+  var query = "cute_kitten,kitten,kitty,cute_cat,cat";
 
-  //First test fetch
+  //TEST FETCH to get maxPageNb and random PageNb for initial fetch
   fetch("https://api.unsplash.com/search/photos/?per_page="+numberOfImages+"&client_id="+clientID+"&query="+query)
     .then(function(data){
       return data.json(); //Get maxPageNumber
     }).then(function(data){
       maxPageNb = data.total_pages;
       pageNb = Math.floor(Math.random() * maxPageNb);
-      fetchedImgUrls = new Array();
-      for(i=0;i<data.results.length;i++){
-        fetchedImgUrls.push(data.results[i].urls.regular)
-      }
   });
+
+  //INITIAL FETCH
+  fetchData("https://api.unsplash.com/search/photos/?page="+pageNb+"&per_page="+numberOfImages+"&client_id="+clientID+"&query="+query);
 
   $("#main-content").click(function(){
     $("#title").css("opacity",0);
 
-    var randomNum = Math.random()*100;
+    var randomNum = Math.random();
 
-    if(randomNum>0.9){ //Show random animals from Unsplash
+    if(randomNum>0.2){ //Show random animals from Unsplash 80% of the time
       count++;
       //console.log("Count : " + count);
 
@@ -51,9 +55,6 @@ $(document).ready(function () {
       }else{console.log("Cannot connect to the API.")}
 
 
-
-
-
       //If on next click you will have to request
       if(count == numberOfImages){
 
@@ -64,26 +65,41 @@ $(document).ready(function () {
         var url = "https://api.unsplash.com/search/photos/?page="+pageNb+"&per_page="+numberOfImages+"&client_id="+clientID+"&query="+query;
 
         //Make a request to the API
-        fetch(url)
-          .then(function(data){
-            return data.json();
-          }).then(function(data){
-              //console.log(data);
-              fetchedImgUrls = new Array();
-              for(i=0;i<data.results.length;i++){
-                fetchedImgUrls.push(data.results[i].urls.regular)
-              }
-              //console.log(fetchedImgUrls);
-              //console.log("Response");
-        });
+        fetchData(url);
       }
     }else{ //Show our cats
+      if(actualLocalImgs.length ==0){
+        actualLocalImgs = [...initialLocalImgs]; //Reset to initial values when all images seen
+      }
 
+      var randomIndex = Math.floor(Math.random() * actualLocalImgs.length);
+
+      $("#main-content").append('<img class="img" src="'+actualLocalImgs[randomIndex]+'">'); //Instantiate the image
+      actualLocalImgs.splice(randomIndex,1); //Remove the image from the list
+
+      if($(".img").length >1){//If there is already an image on the page, remove it
+        $(".img")[0].remove();
+      }
     }
 
 
 
 
   });
+
+  function fetchData(url){
+    fetch(url)
+      .then(function(data){
+        return data.json();
+      }).then(function(data){
+          //console.log(data);
+          fetchedImgUrls = new Array();
+          for(i=0;i<data.results.length;i++){
+            fetchedImgUrls.push(data.results[i].urls.regular)
+          }
+          //console.log(fetchedImgUrls);
+          //console.log("Response");
+    });
+  }
 
 });
